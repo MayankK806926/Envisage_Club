413e38b75801ecc1e9f897323de23cffec304221
Mayank_Budaniya <155288695+MayankK806926@users.noreply.github.com>
2025-03-01 11:44:02 +0530

    Add files via upload

---
+++ VideoStream.py
@@ -0,0 +1,31 @@
+import cv2
+from threading import Thread
+
+class VideoStream:
+    def __init__(self, resolution=(640,480), framerate=30, src=0):
+        self.stream = cv2.VideoCapture(src)
+        self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, resolution[0])
+        self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution[1])
+        self.stream.set(cv2.CAP_PROP_FPS, framerate)
+        
+        (self.grabbed, self.frame) = self.stream.read()
+        self.stopped = False
+
+    def start(self):
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        while True:
+            if self.stopped:
+                self.stream.release()
+                return
+
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+        return self.frame
+
+    def stop(self):
+        self.stopped = True
+
